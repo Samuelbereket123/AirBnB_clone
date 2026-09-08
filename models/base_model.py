327b553c39c5062dc0cfb4c59187ad8aa1b9810c
samuelbereket123 <samuelbereket123@gmail.com>
2026-09-08 23:42:30 +0300

    idk

---
+++ models/base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Represents the BaseModel for all other classes in the AirBnB clone."""
+
+    def __init__(self):
+        """Initialize a new BaseModel instance."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return the string representation of the BaseModel instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary representation of the BaseModel instance."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
