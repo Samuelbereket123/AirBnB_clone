46f2910c6d0a1a6f4d50c1c80d717b3bc4657820
samuelbereket123 <samuelbereket123@gmail.com>
2026-09-09 00:22:21 +0300

    party rock

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,7 +5,12 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file to instances."""
+    """Serializes instances to a JSON file and deserializes JSON file.
+
+    Attributes:
+        __file_path (str): Path to the JSON file.
+        __objects (dict): Dictionary storing objects by <class name>.id.
+    """
 
     __file_path = "file.json"
     __objects = {}
@@ -24,17 +29,17 @@ class FileStorage:
         obj_dict = {
             key: obj.to_dict() for key, obj in FileStorage.__objects.items()
         }
-        with open(FileStorage.__file_path, "w") as f:
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             json.dump(obj_dict, f)
 
     def reload(self):
         """Deserialize the JSON file to __objects (only if file exists)."""
         try:
-            with open(FileStorage.__file_path, "r") as f:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
                 obj_dict = json.load(f)
-                for key, val in obj_dict.items():
+                for val in obj_dict.values():
                     class_name = val["__class__"]
                     if class_name == "BaseModel":
-                        FileStorage.__objects[key] = BaseModel(**val)
+                        self.new(BaseModel(**val))
         except FileNotFoundError:
             pass
