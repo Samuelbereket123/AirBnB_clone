3e38f1445f9d3acd7db52b53cde9b5d4d3493501
samuelbereket123 <samuelbereket123@gmail.com>
2026-09-13 18:52:54 +0300

    finally week 2 is done

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module for the entry point of the command interpreter."""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for the AirBnB clone project."""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print()
+        return True
+
+    def emptyline(self):
+        """An empty line + ENTER shouldn't execute anything."""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
