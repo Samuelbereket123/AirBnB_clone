0b94e4ef88e469e2e792919a2f0d1049b28c683d
samuelbereket123 <samuelbereket123@gmail.com>
2026-09-13 19:10:28 +0300

    1

--- console.py
+++ console.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """Module for the entry point of the command interpreter."""
 import cmd
+import shlex
+from models import storage
+from models.base_model import BaseModel
+
+CLASSES = {
+    "BaseModel": BaseModel
+}
 
 
 class HBNBCommand(cmd.Cmd):
@@ -20,6 +27,131 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """An empty line + ENTER shouldn't execute anything."""
         pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it and prints the id."""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in CLASSES:
+            print("** class doesn't exist **")
+            return
+
+        new_instance = CLASSES[args[0]]()
+        new_instance.save()
+        print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on class and id."""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in CLASSES:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        all_objects = storage.all()
+        if key not in all_objects:
+            print("** no instance found **")
+        else:
+            print(all_objects[key])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id."""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in CLASSES:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        all_objects = storage.all()
+        if key not in all_objects:
+            print("** no instance found **")
+        else:
+            del all_objects[key]
+            storage.save()
+
+    def do_all(self, arg):
+        """Prints string representations of all instances or specified class."""
+        args = shlex.split(arg)
+        all_objects = storage.all()
+        obj_list = []
+
+        if len(args) > 0:
+            if args[0] not in CLASSES:
+                print("** class doesn't exist **")
+                return
+            for key, obj in all_objects.items():
+                if key.startswith(args[0] + "."):
+                    obj_list.append(str(obj))
+        else:
+            for obj in all_objects.values():
+                obj_list.append(str(obj))
+
+        print(obj_list)
+
+    def do_update(self, arg):
+        """Updates an instance based on class name and id by adding/updating attribute."""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in CLASSES:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        all_objects = storage.all()
+        if key not in all_objects:
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        attr_name = args[2]
+        attr_val = args[3]
+
+        if attr_name in ["id", "created_at", "updated_at"]:
+            return
+
+        obj = all_objects[key]
+
+        if hasattr(obj, attr_name):
+            attr_type = type(getattr(obj, attr_name))
+            try:
+                cast_val = attr_type(attr_val)
+            except (ValueError, TypeError):
+                cast_val = attr_val
+        else:
+            if attr_val.isdigit():
+                cast_val = int(attr_val)
+            else:
+                try:
+                    cast_val = float(attr_val)
+                except ValueError:
+                    cast_val = attr_val
+
+        setattr(obj, attr_name, cast_val)
+        obj.save()
 
 
 if __name__ == '__main__':
