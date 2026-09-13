ddc296e04e171644e25ca1097b66f3fb7611679a
samuelbereket123 <samuelbereket123@gmail.com>
2026-09-13 19:21:58 +0300

    Add State, City, Amenity, Place, Review models and update storage and console

--- console.py
+++ console.py
@@ -5,10 +5,20 @@ import shlex
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 CLASSES = {
     "BaseModel": BaseModel,
-    "User": User
+    "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review
 }
 
 
